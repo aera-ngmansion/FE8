3e28dec986342be12e9489c7ef2a2403aed3cbf1
aera <eliwooood@gmail.com>
2017-02-26 23:05:49 +0900

    救出した友軍と交換が出る問題修正

--- hokaku/hokaku/Trade_Jouken.asm
+++ hokaku/hokaku/Trade_Jouken.asm
@@ -2,11 +2,11 @@
 @org	$08022f04
 	;同行者がいる場合は敵ユニットでも交換対象
 	lsl	r0, r0, #25
-	bmi	$08022F48
-	ldrb	r0, [r2, #27]
+	bmi	$08022F48	;敵ならジャンプ
+	ldrb	r0, [r2, #27]	;同行者ロード
 	lsl	r1, r0, #24
 	bmi	check
-	lsl	r1, r0, #23
+	lsl	r1, r0, #25
 	bmi	$08022F2E	;友軍ジャンプ
 	beq	$08022F2E
 check
@@ -16,5 +16,5 @@ check
 	ldrb	r0, [r0, #0x1E]
 	ldrb	r1, [r2, #0x1E]
 	orr	r0, r1
-	bne	$08022f3c	;道具有無チェック
+	bne	$08022f3c	;道具有るならジャンプ
 	b	$08022f2E

96c92c67c893462ff5443bfa89aea9a706e46a1d
ngmansion <eliwooood@gmail.com>
2017-08-15 12:48:49 +0900

    add

--- RePreparation/c0p.asm
+++ RePreparation/c0p.asm
@@ -1,4 +1,8 @@
 @thumb
+	push {lr, r4, r5, r6}
+	mov  r4, r1
+	mov  r5, r2
+	mov  r6, r3
 	
 	ldr  r0, =$0202BCEC
 	ldrb r0, [r0, #14]
@@ -8,9 +12,10 @@
 	add  r0, #0x72
 	ldrb r0, [r0]
 ;mov	r0, #52
-	ldr  r1, =$08002424
+	ldr  r1, =$08002938
 	mov  lr, r1
-	mov  r1, #0
+	mov  r1, r4
+	mov  r2, r5
+	mov  r3, r6
 	@dcw $F800
-	ldr  r0, =$080b692a
-	mov  pc, r0
+	pop {pc, r4, r5, r6}

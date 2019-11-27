f68ba912547a86fdc735dcc4c8628f7e80147e8b
aera <eliwooood@gmail.com>
2019-11-27 22:52:24 +0900

    魔力修正

--- magic/magic/growth/boost.s
+++ magic/magic/growth/boost.s
@@ -8,10 +8,11 @@ RETURN_ADR = (0x08017ddc)
     ldrb r0, [r3, #18]
     strb r0, [r4, #25]
 @魔力
-    add r1, #50
-    ldrb r0, [r1]    @ユニット魔力
-    add r3, #81
-    ldrb r1, [r3]
+    ldr r1, [r4, #4]
+    add r1, #80
+    ldrb r0, [r1]    @兵種魔力
+    add r3, #50
+    ldrb r1, [r3]    @ユニット魔力
     add	r0, r0, r1
     strb r0, [r4, #26]
 @END

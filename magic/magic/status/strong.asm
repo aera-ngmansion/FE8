fe568952a388636a986971fa07fdb699e35cc8be
aera <eliwooood@gmail.com>
2020-01-13 03:30:41 +0900

    FE8N専用化

--- magic/magic/status/strong.asm
+++ magic/magic/status/strong.asm
@@ -1,14 +1,4 @@
 @thumb
-	push	{r4, lr}
-	ldr	r4, =$080168D0
-	mov	lr, r4
-	mov	r4, r0
-	@dcw	$F800
-	lsl	r0, r0, #16
-	lsr	r0, r0, #16
-	ldr	r2, =$080161C8
-	mov	lr, r2
-	@dcw	$F800
 	mov	r1, r0
 	mov	r0, #20
 	ldsb	r0, [r4, r0]

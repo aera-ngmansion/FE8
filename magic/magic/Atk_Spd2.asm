f33d6e3faddc2c6aba28a199d9855bc3055fd60e
aera <eliwooood@gmail.com>
2020-01-26 12:49:08 +0900

    攻速計算改善

--- magic/magic/Atk_Spd2.asm
+++ magic/magic/Atk_Spd2.asm
@@ -1,20 +1,10 @@
 @thumb
+@org	$0802aae4
 	push	{r4, lr}
 	mov	r4, r0
 	add	r0, #74
 	ldrh	r0, [r0, #0]
-		ldr	r1, =$080173b4
-		mov	lr, r1
-		@dcw	$F800
-	ldrb	r1, [r1, #7]
-	cmp	r1, #7
-	bgt	none
-	add	r1, #40
-	ldrb	r1, [r1, r4]
-	cmp	r1, #250
-	bls	none
-	mov	r0, #0
-none
+	bl	$080173b4	;r0に武器の重さ
 	mov	r1, r0
 	
 	ldr	r0, [r4, #4]
@@ -50,6 +40,4 @@ jump
 	mov	r0, #0
 	strh	r0, [r1, #0]
 end
-	pop	{r4}
-	pop	{r0}
-	bx	r0
+	pop	{r4, pc}
